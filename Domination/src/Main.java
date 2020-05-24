94ed5f3f372ec41c606825c269fab69138bc3adb
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-05-25 01:29:26 +0700

    basically done

--- Domination/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    // attempt to create a singleton object?
-    static World world = new World();
-    
-    public static void main(String args []){
-    	world.playfirstRound();
-    }  
-}
