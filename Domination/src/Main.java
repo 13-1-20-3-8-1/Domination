1ae8d573df41bcec4935e46ff51a87dc97a111a5
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-05-24 21:46:09 +0700

    reinforce gitignore

---
+++ Domination/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    // attempt to create a singleton object?
+    static World world = new World();
+    
+    public static void main(String args []){
+    	world.playfirstRound();
+    }  
+}
